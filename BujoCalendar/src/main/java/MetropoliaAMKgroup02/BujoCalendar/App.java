bd5c81681f3b76dbf2a40e4f4f327fcd60eb069d
Vladimir Gavrilov <vladimir.gavrilov@hotmail.com>
2019-09-04 23:11:29 +0300

    App java muokattu

--- BujoCalendar/src/main/java/MetropoliaAMKgroup02/BujoCalendar/App.java
+++ BujoCalendar/src/main/java/MetropoliaAMKgroup02/BujoCalendar/App.java
@@ -10,6 +10,7 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println("Commit toimi");
+	System.out.println("Java koodi onnistui päivittää");
 
     }
 }

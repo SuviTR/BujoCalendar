f755d69645730893dea9b69608c4cdd42f52164f
Vladimir Gavrilov <vladimir.gavrilov@hotmail.com>
2019-09-05 13:43:37 +0300

    backend kansio lisätty

---
+++ BujoCalendar/src/main/java/MetropoliaAMKgroup02/backend/Merkinta.java
@@ -0,0 +1,31 @@
+package MetropoliaAMKgroup02.BujoCalendar;
+
+/**
+ * Vladimir 
+ *
+ */
+public class Merkinta 
+{
+	public enum prioriteetti {small, middle, high};
+
+	public void Add(){
+			
+	}
+
+	public void Delete(){
+
+	}
+
+	public void Edit(){
+
+	}
+
+	public void SetPriority(){
+
+	}
+
+	public void Opiration(){
+
+	}	
+
+}

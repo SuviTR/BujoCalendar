ccc4c7b463cf9a9d15f1cc178968dd9258a4a6e6
Suvi <suvi.rannisto@gmail.com>
2019-10-02 12:23:20 +0300

    Lisätty MenuTest
    Menulle tyhjä JUnit testi.

---
+++ BujoCalendar/src/test/java/MetropoliaAMKgroup02/BujoCalendar/MenuTest.java
@@ -0,0 +1,14 @@
+package MetropoliaAMKgroup02.BujoCalendar;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MenuTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
